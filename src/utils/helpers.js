eeb066f0e66f225b7fc3cfae74367d74f4398876
grsmith35 <gordonrileysmith@gmail.com>
2022-02-18 12:35:09 -0700

    deploy to gh pages

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,69 +1,9 @@
-import * as FileSaver from 'file-saver';
-import * as XLSX from 'xlsx';
 import { jsPDF } from 'jspdf';
 import bgimg from '../assets/images/blue-bar (2).png';
 
 export function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-
-export function cleanName(string) {
-  let nameArr = {fName: "", lName: ""};
-  switch(string) {
-    case 'octayvialong':
-      nameArr.fName = "Octavia";
-      nameArr.lName = "Long";
-      return nameArr;
-
-    case 'RANDOLPHTRYON':
-      nameArr.fName = "Randolph";
-      nameArr.lName = "Tryon";
-        return nameArr;
-
-    case 'BriannaGarcia1':
-      nameArr.fName = "Brianna";
-      nameArr.lName = "Garcia";
-      return nameArr;
-
-    case 'JustinMcNemar':
-      nameArr.fName = "Justin";
-      nameArr.lName = "McNemar";
-      return nameArr;
-
-    case 'LaConstanceKing':
-      nameArr.fName = "LaConstance";
-      nameArr.lName = "King"
-      return nameArr;
-
-    case 'ZabryannaMarieBullard':
-      nameArr.fName = "Zabryana Maria";
-      nameArr.lName = "Bullard";
-      return nameArr;
-
-    default:
-      let newName = string.charAt(0).toLowerCase() + string.slice(1);
-      let location = newName.search(/([A-Z])/g)
-      nameArr.fName = string.slice(0, location);
-      nameArr.lName = string.slice(location);
-      return nameArr;
-  }
-}
- export function fileWriter(arr, check) {
-  const currentDate = Date.now();
-  const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-  const fileExtension = '.xlsx';
-  let fileName ="";
-  if(check === "conversion") {
-    fileName = `${currentDate} conversion calls`;
-  } else {
-    fileName = `${currentDate} time station`;
-  }
-  const ws = XLSX.utils.json_to_sheet(arr);
-  const wb = {Sheets: { 'data': ws }, SheetNames: ['data']};
-  const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-  const data = new Blob([excelBuffer], { type: fileType});
-  FileSaver.saveAs(data,fileName+fileExtension);
- };
 
  export function nameTrimmer(name) {
   const spaceLoc = name.indexOf(" ");
@@ -76,6 +16,11 @@ export function cleanName(string) {
  //formatNumber = (number) => {
 
  //};
+
+ export function locationChecker(number, type) {
+   const limit = 285;
+   
+ }
 
  export function createResume(toSend) {
   let currDesc = 0;
